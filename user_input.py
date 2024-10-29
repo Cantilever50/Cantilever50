e559ea0f25e0a234d83b07b5d313ded2464495db
Cantilever50 <111823740+Cantilever50@users.noreply.github.com>
2024-10-29 08:56:44 +0100

    A python code to take users input- PLP project

---
+++ user_input.py
@@ -0,0 +1,23 @@
+# enter your name
+
+name = input("please enter your name: ")
+
+print(f"\nMy name is: {name}\n")
+
+
+
+# enter your age
+
+age = input("please enter your age: ")
+
+print(f"\nI am {age} years old\n")
+
+
+# enter your location
+
+location = input(" where do you stay ?: ")
+
+print(f"\nI stay at {location} \n")
+
+
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
